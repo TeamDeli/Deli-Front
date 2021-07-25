17274ce98e88af693e494c24b68c655655b1ad47
kiiiyeon <rusl0325@sookmyung.ac.kr>
2021-07-26 01:31:00 +0900

    update: 프로필 사용자 정보 컴포넌트 수정

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,7 +1,12 @@
 import React from 'react';
-//import Styled from "styled-components";
+import Styled from "styled-components";
 import UserInfo from './UserInfo';
 import defaultImage from '../../user.svg'
+
+
+const ProfileWrap = Styled.div`
+
+`;
 
 const Profile = () => {
     //dummyData, 나중에 서버 통신으롣 데이터 받아오는 코드로 수정
@@ -13,9 +18,10 @@ const Profile = () => {
         gender: "여자", //나중에 boolean으로 바꾸기
         birth: "98.03.25"
     }
+
     return(
         <UserInfo userInfo={userInfo}></UserInfo>
-        //UserInfo 밑에 캘린더 버튼 추가
+        
     );
 }
 
