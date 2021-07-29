4385b87d564054a2072c7ae8e3c1963f281cab01
kiiiyeon <rusl0325@sookmyung.ac.kr>
2021-07-29 20:46:03 +0900

    feat: router 연결

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
 //import Styled from 'styled-components';
-import Profile from './components/profile/Profile';
+import Profile from "./pages/Profile";
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Profile />
+    <>
+    <BrowserRouter>
+      <Switch>
+        <Route
+          exact
+          path="/profile"
+          component={() => <Profile></Profile>}
+        ></Route>
+      </Switch>
+    </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
