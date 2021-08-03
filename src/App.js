203ee2156c721cfbdf4fe4e870ea00ae1863828e
kiiiyeon <rusl0325@sookmyung.ac.kr>
2021-08-04 04:27:28 +0900

    feat: 라우터 연결

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
 //import Styled from 'styled-components';
 import AddFriends from "./pages/AddFriends";
+import Profile from "./pages/Profile";
 import Test from "./pages/Test";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <Test></Test>;
+  return (
+    <>
+    <BrowserRouter>
+      <Switch>
+        <Route
+          exact
+          path="/profile"
+          component={() => <Profile></Profile>}
+        ></Route>
+      </Switch>
+    </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
