647c06617c64d1ff412597b8b0bcfd05497b95c1
kyungminlee-12 <min2000.lee@gmail.com>
2021-08-08 17:23:48 +0900

    fix: router와 NavBar 내용 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 //import Styled from 'styled-components';
 
 import Product from "./pages/Product";
+import NavBar from "./components/NavBar/NavBar";
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
+      <NavBar/>
       <Switch>
         <Route
           exact
