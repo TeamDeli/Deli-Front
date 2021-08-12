804f4ba3d24c55312dc765d36c5cf2feca4a123f
Kiltae <loverpleasestayo0@gmail.com>
2021-08-12 19:57:00 +0900

    feat: #14 취소버튼 수정

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -5,99 +5,69 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import InteractionPlugin from "@fullcalendar/interaction";
 import { useState, useRef } from "react";
 import { AddEventModal } from "../components/calendar/AddEventModal";
-import { RemoveEventModal } from "../components/calendar/RemoveEventModal";
 import styled from "styled-components";
-import { FiEdit } from "react-icons/fi";
+import { FaRegCalendarPlus } from "react-icons/fa";
 import { removeElement } from '@fullcalendar/core';
 
 const events1 = [
   {
-    id: 123,
-    title: "운동하기",
-    start: "2021-08-03",
-    end: "2021-08-03",
+    id: 1,
+    title: "광복절",
+    start: "2021-08-15",
+    color: "#fd6f22"
   },
 ];
+
+const CalendarWrapper=styled.div`
+  display:flex;
+  justify-content: center;
+  padding: 30px 0px 0px 0px;
+  align-items: self-start;
+`;
 
 const AddBtn = styled.button`
   color: black;
   background: white;
   border: white;
-  font-size: 3rem;
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  justify-content: flex-end;
+  font-size: 2.5rem;
+  padding: 0 0 0 10px;
+  cursor: pointer;
 `;
 
 function Calendar() {
   const [modalOpen, setModalOpen] = useState(false);
-  const [removeModalOpen, removeSetModalOpen] = useState(false);
   const calendarRef = useRef(null);
-  let newCalendar = new FullCalendar;
   const onEventAdded = (event) => {
     let calendarApi = calendarRef.current.getApi();
     console.log(calendarApi);
     calendarApi.addEvent(event);
   };
 
-  /*  const onEventDelet = (event) => {
-      return event;
-    }; */
   return (
-
-    <div>
-      <div>
-        <RemoveEventModal
-          isOpen={removeModalOpen}
-          onClose={() => {
-            removeSetModalOpen(false);
-          }
-          }
-          onRemove={() => {
-            removeSetModalOpen(false);
-          }
-          }
-        ></RemoveEventModal>
-      </div>
-
-      <div style={{ position: "relative", zIndex: 0 }}>
+    <div style={{position:"relative",zIndex:0}}>
+      <CalendarWrapper>
         <FullCalendar
           ref={calendarRef}
           plugins={[dayGridPlugin, timeGridPlugin, InteractionPlugin]}
           initialView="dayGridMonth"
-          headerToolbar={{
-            center: "dayGridMonth,timeGridWeek,timeGridDay",
-          }}
+          height={590}
           events={events1}
-          contentHeight={600}
-          dateClick={(e) => console.log(e.dateStr)}
           eventClick={(e) => {
-            /* removeSetModalOpen(true); //삭제하겠습니까? 모달창 열고 삭제 버튼 누를 때 이벤트 삭제하는 부분
-             onEventDelet(e);
-             console.log(e);
-             console.log(e.event);
-             rrmove.EventApi=e;
-             console.log(rrmove.EventApi)*/
-            e.event.remove();
+            if(window.confirm(`${e.event.title}를 삭제하시겠습니까?`)){
+              e.event.remove();
+            }
           }}
         />
-      </div>
-      <div>
+        <AddBtn onClick={() => setModalOpen(true)}>
+          <FaRegCalendarPlus />
+        </AddBtn>
+      </CalendarWrapper>
         <AddEventModal
           isOpen={modalOpen}
           onClose={() => setModalOpen(false)}
           onEventAdded={(event) => onEventAdded(event)}
         ></AddEventModal>
-      </div>
-      <Wrapper>
-        <AddBtn onClick={() => setModalOpen(true)}>
-          <FiEdit />
-        </AddBtn>
-      </Wrapper>
     </div>
-
   );
 }
 export default Calendar;
