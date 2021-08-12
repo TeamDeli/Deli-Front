c8c08d146170ac26586558fb5ebbe3785e628216
Kiltae <loverpleasestayo0@gmail.com>
2021-08-12 19:53:59 +0900

    chore:페이지 테스트

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
-//import Styled from 'styled-components';
 import AddFriends from "./pages/AddFriends";
 import Test from "./pages/Test";
 import "./App.css";
 import Calendar from './pages/Calendar';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LandingPage from './pages/LandingPage/LandingPage'
+import NavBar from './components/NavBar/NavBar'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <Calendar></Calendar>;
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+        <Route
+          exact
+          path="/calendar"
+          component={() => <Calendar></Calendar>}
+        ></Route>
+      </Switch>
+    </Router>
+  );
 }
 export default App;
